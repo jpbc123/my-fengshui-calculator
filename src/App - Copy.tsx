d7707fc3b9a3e7ce20f31056511ccc75d51a9188
jpbc123 <jpbcatindig@outlook.com>
2025-09-10 13:55:18 +0800

    Add Vercel Analytics

--- src/App - Copy.tsx
+++ src/App - Copy.tsx
@@ -49,6 +49,7 @@ import Store from "./pages/Store";
 
 import ScrollToTop from "@/components/ScrollToTop";
 import Footer from "@/components/Footer";
+import PageTitleManager from "@/components/PageTitleManager";
 
 const queryClient = new QueryClient();
 
@@ -59,75 +60,76 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <ScrollToTop />
+        <PageTitleManager>
+          {/* App layout wrapper */}
+          <div className="flex flex-col min-h-screen">
+            <main className="flex-1">
+              <Routes>
+                <Route path="/" element={<Index />} />
+                
+                {/* Main Menus */}
+                <Route path="/feng-shui" element={<FengShui />} />
+                <Route path="/numerology" element={<Numerology />} /> 
+                <Route path="/astrology" element={<Astrology />} />
+                <Route path="/horoscope" element={<Horoscope />} />
+                <Route path="/store" element={<Store />} /> 
+                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/contact-us" element={<ContactUs />} />
+                
+                {/* Submenu-Feng Shui */}
+                <Route path="/personal-element" element={<PersonalElement />} />
+                <Route path="/kua-number-calculator" element={<KuaNumberCalculator />} />
+                
+                {/* Submenu-Numerology */}
+                <Route path="/visiber-calculator" element={<VisiberCalculator />} />
+                
+                {/* Submenu-Astrology */}
+                <Route path="/chinese-zodiac-calculator" element={<ChineseZodiacCalculator />} />
+                <Route path="/chinese-zodiac-landing" element={<ChineseZodiacLanding />} />
+                <Route path="/western-zodiac-calculator" element={<WesternZodiacCalculator />} />
+                
+                {/* Features-Horoscopes */}
+                <Route path="/zodiac/:zodiac" element={<ChineseHoroscopeResult />} />
+                <Route path="/western-horoscope" element={<WesternDailyHoroscope />} />
+                
+                {/* Tools Section */}
+                <Route path="/daily-wisdom-article" element={<DailyWisdomArticlePage />} />
+                <Route path="/aura-analysis" element={<AuraAnalysisPage />} />
+                <Route path="/planetary-overview" element={<PlanetaryOverviewPage />} />
+                
+                {/* Games Section */}
+                <Route path="/games-fun" element={<GamesFunLanding />} />
+                <Route path="/lucky-numbers" element={<GameLuckyNumber />} />
+                <Route path="/name-compatibility" element={<GameNameCompatibility />} />
+                <Route path="/chinese-compatibility" element={<GameChineseCompatibility />} />
+                <Route path="/western-compatibility" element={<GameWesternCompatibility />} />
+                <Route path="/fortune-cookie" element={<GameFortuneCookie />} />
+                
+                {/* Meditation */}
+                <Route path="/meditation" element={<MeditationLanding />} />
+                <Route path="/meditate-visualization" element={<MeditateVisualizationExercise />} />
+                <Route path="/meditate-yoga-pose" element={<MeditateYogaPose />} />
+                <Route path="/meditate-affirmation" element={<MeditateAffirmation />} />
+                <Route path="/meditate-morning" element={<MeditateMorningMindfulness />} />
+                <Route path="/meditate-evening" element={<MeditateEveningRelaxation />} />
+                
+                {/* Articles Routing */}
+                <Route path="/article" element={<ArticleMainPage />} />
+                <Route path="/articles/:slug" element={<ArticlePage />} />
 
-        {/* App layout wrapper */}
-        <div className="flex flex-col min-h-screen">
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              
-              {/* Main Menus */}
-              <Route path="/feng-shui" element={<FengShui />} />
-              <Route path="/numerology" element={<Numerology />} /> 
-              <Route path="/astrology" element={<Astrology />} />
-              <Route path="/horoscope" element={<Horoscope />} />
-              <Route path="/store" element={<Store />} /> 
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route path="/about-us" element={<AboutUs />} />
-			  <Route path="/contact-us" element={<ContactUs />} />
-              
-              {/* Submenu-Feng Shui */}
-              <Route path="/personal-element" element={<PersonalElement />} />
-              <Route path="/kua-number-calculator" element={<KuaNumberCalculator />} />
-              
-              {/* Submenu-Numerology */}
-              <Route path="/visiber-calculator" element={<VisiberCalculator />} />
-              
-              {/* Submenu-Astrology */}
-              <Route path="/chinese-zodiac-calculator" element={<ChineseZodiacCalculator />} />
-              <Route path="/chinese-zodiac-landing" element={<ChineseZodiacLanding />} />
-              <Route path="/western-zodiac-calculator" element={<WesternZodiacCalculator />} />
-              
-              {/* Features-Horoscopes */}
-              <Route path="/zodiac/:zodiac" element={<ChineseHoroscopeResult />} />
-              <Route path="/western-horoscope" element={<WesternDailyHoroscope />} />
-              
-              {/* Tools Section */}
-              <Route path="/daily-wisdom-article" element={<DailyWisdomArticlePage />} />
-              <Route path="/aura-analysis" element={<AuraAnalysisPage />} />
-              <Route path="/planetary-overview" element={<PlanetaryOverviewPage />} />
-              
-              {/* Games Section */}
-			  <Route path="/games-fun" element={<GamesFunLanding />} />
-              <Route path="/lucky-numbers" element={<GameLuckyNumber />} />
-              <Route path="/name-compatibility" element={<GameNameCompatibility />} />
-              <Route path="/chinese-compatibility" element={<GameChineseCompatibility />} />
-              <Route path="/western-compatibility" element={<GameWesternCompatibility />} />
-              <Route path="/fortune-cookie" element={<GameFortuneCookie />} />
-			  
-			  {/* Meditation */}
-			  <Route path="/meditation" element={<MeditationLanding />} />
-              <Route path="/meditate-visualization" element={<MeditateVisualizationExercise />} />
-			  <Route path="/meditate-yoga-pose" element={<MeditateYogaPose />} />
-			  <Route path="/meditate-affirmation" element={<MeditateAffirmation />} />
-			  <Route path="/meditate-morning" element={<MeditateMorningMindfulness />} />
-			  <Route path="/meditate-evening" element={<MeditateEveningRelaxation />} />
-              
-              {/* Articles Routing */}
-              <Route path="/article" element={<ArticleMainPage />} />
-              <Route path="/articles/:slug" element={<ArticlePage />} />
+                {/* Coming Soon */}
+                <Route path="/community-chat" element={<ComingSoonPage />} />
+                <Route path="/coming-store" element={<ComingSoonStore />} />
+                
+                {/* Catch-all route */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
 
-              {/* Coming Soon */}
-              <Route path="/community-chat" element={<ComingSoonPage />} />
-              <Route path="/coming-store" element={<ComingSoonStore />} />
-              
-              {/* Catch-all route */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-
-          <Footer />
-        </div>
+            <Footer />
+          </div>
+        </PageTitleManager>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
